436642cedce46c080bc5678602c1569d76d59114
andisa01 <a.andis@yale.edu>
2025-06-06 21:17:30 -0400

    Updating to property tac calulation with phase-in. Adding variables to set phase-in year and term. Updating language to reflect phase-in.

--- scripts/prototyping.R
+++ scripts/prototyping.R
@@ -2,23 +2,28 @@ library(tidyverse)
 
 df_raw <- read_rds("data/all_streets_data_4_18_25.rds")
 
-mill_rate_new <- 46.61
+mill_rate_new <- 52.16
 mill_rate_old <- 55.61
+phase_in_year <- 1
+phase_in_term <- 4
 
 df <- df_raw |>
-  mutate(assessment_new = appraisal * 0.7,
-         assessment_old = hist_total_value * 0.7,
-         property_tax_new = assessment_new * mill_rate_new / 1000 |> round(),
-         property_tax_old = assessment_old * mill_rate_old / 1000 |> round())
+  mutate(
+    assessment_new = appraisal * 0.7,
+    assessment_old = hist_total_value * 0.7,
+    property_tax_new =  
+      (assessment_old + ((assessment_new - assessment_old)*(phase_in_year/phase_in_term))) * mill_rate_new / 1000 |> round(),
+    property_tax_old = assessment_old * mill_rate_old / 1000 |> round()
+  )
 
 df <- df |>
   select(address, property_tax_new, property_tax_old, appraisal, assessment_new, hist_total_value, assessment_old) |>
   rename(appraisal_new = appraisal,
          appraisal_old = hist_total_value)
 
-write_rds(df, file = "data/all_streets_data_4_18_25_cleaned.rds")
+# write_rds(df, file = "data/all_streets_data_4_18_25_cleaned.rds")
 
-example_st <- "32 LANSDOWNE AVE"
+example_st <- "83 GREENWAY ST"
 
 df_home <- df |>
   filter(address == example_st)
@@ -38,7 +43,20 @@ appraisal_new <- appraisal_new |> formatC(format="d", big.mark=",")
 appraisal_old <- appraisal_old |> formatC(format="d", big.mark=",")
 monthly_payment <- formatC(monthly_payment, digits = 2, format = "f")
 
-string1 <- str_c("With the proposed mill rate of 55.61, your property taxes will be $",
+# adding phase in term language
+phase_in_year_natural_lang <- 
+  case_when(
+    phase_in_year == 1 ~ "1st",
+    phase_in_year == 2 ~ "2nd",
+    phase_in_year == 3 ~ "3rd",
+    phase_in_year == 4 ~ "4th"
+  )
+
+string1 <- str_c("With the proposed mill rate of 52.1, in the ",
+                 phase_in_year_natural_lang,
+                 " year of the ", 
+                 phase_in_term,
+                 "-year reevaluation phase-in, your property taxes will be $",
                  prop_tax_new,
                  " based on the current value of your home of $",
                  appraisal_new, ".")
@@ -53,6 +71,8 @@ string3 <- str_c("Your property taxes will increase by $",
                  "% increase, and you will pay an extra $",
                  monthly_payment,
                  " per month in taxes.")
+
+paste(string1, string2, string3, sep = " ")
 
 df_home <- df_home |>
   rename(`Proposed New Property Tax` = property_tax_new,
@@ -103,18 +123,18 @@ df_home |>
 
 email_string <- 
   str_c(
-  "To Mayor Lauren Garret and the Hamden Legislative Council Members, ",
-  "I am writing as a homeowner in Hamden, CT, to urge you to reconsider the proposed mill rate of 46.61 ",
-  "and instead adopt a significantly lower rate—ideally in the mid-to-low 30s. This drastic increase in ",
-  "property taxes would place an overwhelming financial burden on residents, many of whom are already struggling",
-  "with rising costs in nearly every aspect of daily life, from exorbitant grocery prices to some of the highest utility rates in the nation. ",
-  "For my property, the proposed mill rate would increase my property taxes by $", prop_tax_new, ", which is a ", prop_perc_inc, "% increase compared to my 2024 property taxes.\n\n",
-  "Connecticut is already one of the most expensive states to live in, and Hamden ranks among its costliest towns. With the new property appraisals set to take effect in May 2025, many homeowners have seen their home values increase by 40% to 60%. Under the proposed mill rate, the average homeowner’s annual property tax bill would rise over 20%, adding hundreds of dollars to monthly expenses. This is not only an unsustainable financial strain on individual residents but also a significant threat to the town’s long-term economic health.\n\n",
-  "A town thrives when it attracts and retains taxpaying homeowners who invest in and maintain their properties. Excessive property taxes will discourage new residents and businesses from moving to Hamden while forcing out long-time homeowners—especially retirees who may have paid off their mortgages but can no longer afford the tax burden. If this trend continues, Hamden risks declining property values, reduced homeownership, and an eroded sense of community.\n\n",
-  "Homeowners cannot and should not bear the full weight of the town’s financial burdens. This level of taxation is untenable, unfair, and unsustainable. I urge you to consider the broader implications of this decision, not just for individual residents but for the future stability and prosperity of Hamden itself.\n\n",
-  "Please know that we, the homeowners, are paying close attention. The outcome of this vote will not be forgotten when we cast our ballots in the primaries and general elections.\n",
-  "I'm not alone in how I feel—check out this petition to see others who support this change.\n\n",
-  "Sincerely,\n",
-  "[Your Name]\n",
-  "[Your Address]"
-)
+    "To Mayor Lauren Garret and the Hamden Legislative Council Members, ",
+    "I am writing as a homeowner in Hamden, CT, to urge you to reconsider the proposed mill rate of 46.61 ",
+    "and instead adopt a significantly lower rate—ideally in the mid-to-low 30s. This drastic increase in ",
+    "property taxes would place an overwhelming financial burden on residents, many of whom are already struggling",
+    "with rising costs in nearly every aspect of daily life, from exorbitant grocery prices to some of the highest utility rates in the nation. ",
+    "For my property, the proposed mill rate would increase my property taxes by $", prop_tax_new, ", which is a ", prop_perc_inc, "% increase compared to my 2024 property taxes.\n\n",
+    "Connecticut is already one of the most expensive states to live in, and Hamden ranks among its costliest towns. With the new property appraisals set to take effect in May 2025, many homeowners have seen their home values increase by 40% to 60%. Under the proposed mill rate, the average homeowner’s annual property tax bill would rise over 20%, adding hundreds of dollars to monthly expenses. This is not only an unsustainable financial strain on individual residents but also a significant threat to the town’s long-term economic health.\n\n",
+    "A town thrives when it attracts and retains taxpaying homeowners who invest in and maintain their properties. Excessive property taxes will discourage new residents and businesses from moving to Hamden while forcing out long-time homeowners—especially retirees who may have paid off their mortgages but can no longer afford the tax burden. If this trend continues, Hamden risks declining property values, reduced homeownership, and an eroded sense of community.\n\n",
+    "Homeowners cannot and should not bear the full weight of the town’s financial burdens. This level of taxation is untenable, unfair, and unsustainable. I urge you to consider the broader implications of this decision, not just for individual residents but for the future stability and prosperity of Hamden itself.\n\n",
+    "Please know that we, the homeowners, are paying close attention. The outcome of this vote will not be forgotten when we cast our ballots in the primaries and general elections.\n",
+    "I'm not alone in how I feel—check out this petition to see others who support this change.\n\n",
+    "Sincerely,\n",
+    "[Your Name]\n",
+    "[Your Address]"
+  )
