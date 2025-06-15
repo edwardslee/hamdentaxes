6e50357e9b2e7428de3506114c56e4378a56f99d
Ed Lee <edlee@duck.com>
2025-06-15 01:53:28 -0400

    Fixed LC budget numbers; full text added

--- shiny/ui_server_test/app.R
+++ shiny/ui_server_test/app.R
@@ -23,7 +23,25 @@ ui <- fluidPage(
       "
     ))
   ),
-  titlePanel("Budget Projection"),
+  fluidRow(
+    column(12, class = "top-section",
+           h3("2025-2026 (FY26) Property Tax Calculator"),
+           p("This calculator attempts to estimate your property taxes for the next four fiscal years under different budget scenarios that you can choose.
+           It starts with using either the Council's Budget or Mayor Garrett's Budget for FY2026 (i.e., the upcoming year), and then you can decide what the budget totals for the subsequent 3 years.
+           Then, you can choose if a 4-year phase-in of the revaluation is in place, and we provide 3 different scenarios of how the budget can change. 
+           You are also free to manually change the budget totals for each year with your own custom numbers."),
+           br(),
+           p("To estimate your taxes for the next four years:"),
+           tags$li("Search and choose the address of your property"),
+           tags$li("Choose a proposed budget for FY2026 (either the Council's Budget or Mayor Garrett's Budget"),
+           tags$li("Choose if there is a 4-year phase-in of the new valuations"),
+           tags$li("Choose one of the convenient budget projection options", strong("or"), "choose your own total budget numbers for each of the three following years."),
+           br(),
+           p("Please note that these estimates are not official estimates from the town nor are they guaranteed to be accurate.
+           The methodology used for these calculations uses a set of broad assumptions (more details explained at the bottom). 
+           This tool is meant to illustrate the possible changes to your property taxes under different scenarios for the town budget over the next four years.")
+    )
+  ),
   sidebarLayout(
     sidebarPanel(
       # Custom CSS for padding
@@ -78,10 +96,10 @@ ui <- fluidPage(
         selected = "grow"
       ),
       h4("Set the budget totals for the next four years"),
-      textInput("fy2026", "FY2026", value = comma(310653000)),
-      textInput("fy2027", "FY2027", value = comma(325284756)),
-      textInput("fy2028", "FY2028", value = comma(340605668)),
-      textInput("fy2029", "FY2029", value = comma(356648195))
+      textInput("fy2026", "FY2026", value = comma(303790424)),
+      textInput("fy2027", "FY2027", value = comma(319146053)),
+      textInput("fy2028", "FY2028", value = comma(334177832)),
+      textInput("fy2029", "FY2029", value = comma(349917608))
     ),
     mainPanel(
       h4("Estimated Mill Rates For the Next Four Years"),
@@ -107,10 +125,32 @@ ui <- fluidPage(
       ),
       fluidRow(
         column(12,
-               h4("Tax Comparison Across Years (With Phase-In)"),
+               h4("Monthly Property Taxes Across The Next 4 Years"),
+               plotOutput("tax_comparison_monthly"),
+               h4("Annual Property Taxes Across The Next 4 Years"),
                plotOutput("tax_comparison")
         )
       )
+    )
+  ),
+  fluidRow(
+    column(12, class = "top-section",
+           h3("Methodology"),
+           p("This section explains some of the numbers and assumptions used within the code of this calculator. If there are any errors or mistakes, please reach out with details."),
+           tags$li("Grand List (i.e., the net taxable property): the pre-revaluation grand list is  $4,065,443,518, and the post-revaluation grand list is $5,726,470,270. The latter is assumed to be the value of the grand list for all four years if there is no phase-in."),
+           tags$li("Grand List for 4-year phase-in: the grand list is incrementally increased by 25% of the new valuation total with each year [Grand List of Year X = Pre Grand list + X/4 * (Post Grand List - Pre Grand List)]; 
+                   namely, FY2026 grand list would be $4,480,700,206, FY2027 grand list would be  $4,895,956,894, FY2028 grand list would be  $5,311,213,582, and FY2029 grand list would be  $5,726,470,270."),
+           tags$li("Property valuation: valuations for properties are obtained from Hamden's online database: https://gis.vgsi.com/Hamdenct/"),
+           tags$li("Property valuation with or without phase-in: if no phase in is selected, the property valuations are the post-revaluation numbers for all four years, and if phase-in is selected, the property value is determined
+                   similarly to how the phase-in grand list is calculated."),
+           tags$li("Annual Property Tax: calculated with the formula Tax = Valuation * 0.7 * Mill Rate / 1000"),
+           tags$li("Mayor's Budget: Mayor Garrett's budget is proposed to have a total of $313,653,000 with a mill rate of 43.39 and a total property tax revenue ask of $248,471,545."),
+           tags$li("Council's Budget: The Council's budget is proposed to have a total of $304,790,424.29 with a mill rate of 52.16 (with the 4-year phase in), which imputes a total property tax revenue ask of $233,713,322.74."),
+           tags$li("Total property tax revenue ask: the total property tax revenue ask is assumed to be 80% of the total budget. This assumption is based on historical data; FY2021 was 80.5%, FY2022 was 82.1%, FY2023 was 80.5%, FY2024 was 79.9%, and FY2025 was 75.2%."),
+           tags$li("Certain mill rates are set manually according to reported values: Council's Budget with no phase-in was estimated to have a mill rate of 39.99, Council's Budget with phase-in was estimated to have a mill rate of 52.16 (both provided by the LC), and the Mayor's Budget with no phase-in was reported by the Mayor's office to be 43.39.
+                   Mill rates with no reported or published number are calculated by dividing the total property tax revenue ask by the grand list of that year and multiplying by 1000: mill rate = property tax revenue ask / grand list * 1000."),
+           tags$li("The default option of 'Budget grows 4.71% yearly' has an increase rate of 4.71% because this is the average yearly growth of budget from FY2021 to FY2026 (using the Mayor's proposed budget for FY2026)"),
+           tags$li("The option of 'Budget decreases 2% yearly' was chosen to have a decrease rate of 2% because it is a nice round number that seems to keep property taxes relatively flat.")
     )
   )
 )
@@ -118,9 +158,9 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   parse_input <- function(x) as.numeric(gsub(",", "", x))
   council_vals <- list(
-    grow     = c(310653000, 325284756, 340605668, 356648195),
-    flat     = rep(310653000, 4),
-    decrease = c(310653000, 304439940, 298351141, 292384118)
+    grow     = c(303790424, 319146053, 334177832, 349917608),
+    flat     = rep(303790424, 4),
+    decrease = c(303790424, 298694616, 292720723, 286866309)
   )
   mayor_vals <- list(
     grow     = c(313653000,  328426056, 343894924, 360092374),
@@ -472,8 +512,58 @@ server <- function(input, output, session) {
                   vjust = -0.5, size = 3.5) +
         geom_text(aes(label = ifelse(is.na(Mill_Rate), "", paste0("Mill Rate: ", formatC(Mill_Rate, digits=2, format="f")))),
                   vjust = 1.5, size = 3) +
-        labs(title = "Property Tax Comparison Across Years (With Phase-In)",
-             y = "Annual Property Tax ($)",
+        labs(y = "Annual Property Tax ($)",
+             x = "") +
+        theme_minimal() +
+        theme(
+          legend.position = "none",
+          plot.title = element_text(hjust = 0.5, face = "bold"),
+          axis.text = element_text(size = 10),
+          panel.grid.major.x = element_blank()
+        ) +
+        scale_fill_brewer(palette = "YlOrRd")
+    }, error = function(e) {
+      plot <- ggplot() +
+        annotate("text", x = 0.5, y = 0.5, label = paste("Error:", e$message)) +
+        theme_void()
+      return(plot)
+    })
+  })
+  
+  output$tax_comparison_monthly <- renderPlot({
+    # browser()
+    req(input$address)
+    tax_data <- calculate_taxes_for_all_years()
+    if (is.null(tax_data)) return(NULL)
+    tryCatch({
+      plot_data <- tibble(
+        Year = c("2024", "Year 1", "Year 2", "Year 3", "Year 4"),
+        Tax = c(
+          as.numeric(tax_data$baseline$tax),
+          as.numeric(tax_data$year1$tax),
+          as.numeric(tax_data$year2$tax),
+          as.numeric(tax_data$year3$tax),
+          as.numeric(tax_data$year4$tax)
+        ),
+        Mill_Rate = c(
+          NA,
+          as.numeric(tax_data$year1$mill_rate),
+          as.numeric(tax_data$year2$mill_rate),
+          as.numeric(tax_data$year3$mill_rate),
+          as.numeric(tax_data$year4$mill_rate)
+        )
+      )
+      plot_data <- plot_data |>
+        mutate(Tax = round(Tax/12))
+      plot_data$Tax[is.na(plot_data$Tax)] <- 0
+      plot_data$Year <- factor(plot_data$Year, levels = c("2024", "Year 1", "Year 2", "Year 3", "Year 4"))
+      ggplot(plot_data, aes(x = Year, y = Tax, group = 1)) +
+        geom_col(aes(fill = Year), width = 0.7) +
+        geom_text(aes(label = paste0("$", formatC(round(Tax), format = "d", big.mark = ","))),
+                  vjust = -0.5, size = 3.5) +
+        geom_text(aes(label = ifelse(is.na(Mill_Rate), "", paste0("Mill Rate: ", formatC(Mill_Rate, digits=2, format="f")))),
+                  vjust = 1.5, size = 3) +
+        labs(y = "Annual Property Tax ($)",
              x = "") +
         theme_minimal() +
         theme(
